4d0329915eed1baccb48458bef37fe89d1642288
anatecla <anatecla333@gmail.com>
2013-01-22 16:25:08 +0100

    Primeros ficheros de store con bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '3381028720e7c4a6dc447d25d5fdfdaa3139e256c418388ea89a74be8f7f7ca91799c24b236afcd8e54a2ccfc528a3417b8f6d9936ab92b3863e279d3d5b4d4b'
